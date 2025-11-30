17ccc18caae15fde4802bb7ebabaaf85aa3d06d2
zolexander <zoltan.jancso91@gmail.com>
2025-11-30 19:57:49 +0100

    KF6: mark files that reference KActivities::Info::State for manual porting

--- app/wm/abstractwindowinterface.cpp
+++ app/wm/abstractwindowinterface.cpp
@@ -1,3 +1,4 @@
+/* KF6-PORT-REVIEW-InfoState: KActivities::Info::State removed in KF6. Replace usage with availability()/isCurrent()/custom flags. */
 /* KF6-PORT-REVIEW-currentActivityChanged: please verify semantics: currentActivityChanged removed in KF6. */
 /*
     SPDX-FileCopyrightText: 2016 Smith AR <audoban@openmailbox.org>
